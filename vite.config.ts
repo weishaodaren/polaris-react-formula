15ac7b030129b27930019cf22cd777ce93e62040
weishaodaren <zhaojiawei@startable.cn>
2022-09-21 13:03:01 +0800

    版本确定

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      jsxRuntime: 'classic',
-    }),
+    react(),
   ],
   build: {
     target: 'chrome63',
