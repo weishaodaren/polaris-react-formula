0d0b62ee72fb02d687a914c3696e08aaf33f9a03
weishaodaren <weishaodaren@gmail.com>
2022-11-06 14:57:39 +0800

    fix chinese

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -2,4 +2,4 @@ import ZH_CN from './zhCN';
 import EN_US from './enUS';
 
 export const language = localStorage.getItem('Language') || navigator.language.slice(0, 2);
-export const Locale = language !== 'zh' ? ZH_CN : EN_US;
+export const Locale = language !== 'zh' ? EN_US : ZH_CN;
